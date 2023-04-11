e324f4bd19073522dd085c8e20b073a048a0cca1
Bruno Almeida de Matos <65208006+BrunoAlm@users.noreply.github.com>
2023-04-11 19:01:58 -0300

    new endpoint and project

--- lib/app/pages/home/cadastrar_dados/widgets/added_equipments.dart
+++ lib/app/pages/home/cadastrar_dados/widgets/added_equipments.dart
@@ -19,142 +19,147 @@ class _AddedEquipmentsState extends State<AddedEquipments> {
     return Expanded(
       child: Padding(
         padding: const EdgeInsets.only(bottom: 100.0),
-        child: ListView.builder(
-            itemCount: widget.equipmentsStore.equipments.length,
-            itemBuilder: (BuildContext context, int index) {
-              var equipment = widget.equipmentsStore.equipments[index];
+        child: SizedBox(
+          width: 350,
+          child: ListView.builder(
+              itemCount: widget.equipmentsStore.equipments.length,
+              itemBuilder: (BuildContext context, int index) {
+                var equipment = widget.equipmentsStore.equipments[index];
 
-              return Card(
-                margin: const EdgeInsets.all(8),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 18,
-                  ),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SizedBox(
-                            width: 180,
-                            child: Text(
-                              equipment.name,
-                              style: Theme.of(context).textTheme.titleMedium,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              widget.equipmentsStore.filteredEquipments.value
-                                  .add(equipment.name);
-                              widget.equipmentsStore.remove(equipment.id);
-                            },
-                            icon: Icon(
-                              Icons.delete_outline,
-                              color: Theme.of(context).colorScheme.error,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 10),
-                      Container(
-                        decoration: const BoxDecoration(),
-                        padding: const EdgeInsets.all(8),
-                        child: Row(
+                return Card(
+                  margin: const EdgeInsets.all(8),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 18,
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Column(
-                              children: [
-                                const Text('Quantidade'),
-                                DropdownButton<int>(
-                                  value: equipment.qty,
-                                  onChanged: (value) {
-                                    equipment.qty = value!;
-                                  },
-                                  underline: const SizedBox(),
-                                  menuMaxHeight: 260,
-                                  itemHeight: 50,
-                                  items: List.generate(
-                                    15,
-                                    (index) {
-                                      return DropdownMenuItem(
-                                        value: index + 1,
-                                        child: Text(
-                                          (index + 1).toString(),
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .labelLarge,
-                                        ),
-                                      );
-                                    },
-                                  ),
-                                ),
-                              ],
+                            SizedBox(
+                              width: 180,
+                              child: Text(
+                                equipment.name,
+                                style: Theme.of(context).textTheme.titleMedium,
+                              ),
                             ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text('Tempo p/ dia'),
-                                const SizedBox(height: 18),
-                                TextButton(
-                                  onPressed: () async {
-                                    equipment.time = await showTimePicker(
-                                      context: context,
-                                      initialTime:
-                                          const TimeOfDay(hour: 00, minute: 00),
-                                      helpText:
-                                          'Quantas horas esse equipamento foi utilizado?',
-                                      cancelText: 'Cancelar',
-                                      confirmText: 'Confirmar',
-                                      hourLabelText: 'Horas',
-                                      minuteLabelText: 'Minutos',
-                                      initialEntryMode:
-                                          TimePickerEntryMode.inputOnly,
-                                    );
-                                    setState(() {});
-                                  },
-                                  child: Text(
-                                    equipment.time?.format(context) ?? '00:00',
-                                  ),
-                                  style: TextButton.styleFrom(
-                                    maximumSize: const Size(100, 50),
-                                    padding: const EdgeInsets.all(8),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                const Text('Potência'),
-                                SizedBox(
-                                  width: 60,
-                                  child: TextField(
-                                    keyboardType: TextInputType.number,
-                                    controller: widget.equipmentsStore
-                                        .equipments[index].power,
-                                    style:
-                                        Theme.of(context).textTheme.labelLarge,
-                                    decoration: InputDecoration(
-                                      hintText: 'kWh',
-                                      hintStyle:
-                                          Theme.of(context).textTheme.bodySmall,
-                                      contentPadding:
-                                          const EdgeInsets.symmetric(
-                                              horizontal: 5),
-                                      border: const UnderlineInputBorder(),
-                                    ),
-                                  ),
-                                )
-                              ],
+                            IconButton(
+                              onPressed: () {
+                                widget.equipmentsStore.filteredEquipments.value
+                                    .add(equipment.name);
+                                widget.equipmentsStore.remove(equipment.id);
+                              },
+                              icon: Icon(
+                                Icons.delete_outline,
+                                color: Theme.of(context).colorScheme.error,
+                              ),
                             ),
                           ],
                         ),
-                      ),
-                    ],
+                        const SizedBox(height: 10),
+                        Container(
+                          decoration: const BoxDecoration(),
+                          padding: const EdgeInsets.all(8),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                children: [
+                                  const Text('Quantidade'),
+                                  DropdownButton<int>(
+                                    value: equipment.qty,
+                                    onChanged: (value) {
+                                      equipment.qty = value!;
+                                    },
+                                    underline: const SizedBox(),
+                                    menuMaxHeight: 260,
+                                    itemHeight: 50,
+                                    items: List.generate(
+                                      15,
+                                      (index) {
+                                        return DropdownMenuItem(
+                                          value: index + 1,
+                                          child: Text(
+                                            (index + 1).toString(),
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .labelLarge,
+                                          ),
+                                        );
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  const Text('Tempo p/ dia'),
+                                  const SizedBox(height: 18),
+                                  TextButton(
+                                    onPressed: () async {
+                                      equipment.time = await showTimePicker(
+                                        context: context,
+                                        initialTime: const TimeOfDay(
+                                            hour: 00, minute: 00),
+                                        helpText:
+                                            'Quantas horas esse equipamento foi utilizado?',
+                                        cancelText: 'Cancelar',
+                                        confirmText: 'Confirmar',
+                                        hourLabelText: 'Horas',
+                                        minuteLabelText: 'Minutos',
+                                        initialEntryMode:
+                                            TimePickerEntryMode.inputOnly,
+                                      );
+                                      setState(() {});
+                                    },
+                                    child: Text(
+                                      equipment.time?.format(context) ??
+                                          '00:00',
+                                    ),
+                                    style: TextButton.styleFrom(
+                                      maximumSize: const Size(100, 50),
+                                      padding: const EdgeInsets.all(8),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  const Text('Potência'),
+                                  SizedBox(
+                                    width: 60,
+                                    child: TextField(
+                                      keyboardType: TextInputType.number,
+                                      controller: widget.equipmentsStore
+                                          .equipments[index].power,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .labelLarge,
+                                      decoration: InputDecoration(
+                                        hintText: 'kWh',
+                                        hintStyle: Theme.of(context)
+                                            .textTheme
+                                            .bodySmall,
+                                        contentPadding:
+                                            const EdgeInsets.symmetric(
+                                                horizontal: 5),
+                                        border: const UnderlineInputBorder(),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              );
-            }),
+                );
+              }),
+        ),
       ),
     );
   }
