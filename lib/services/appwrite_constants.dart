e8077e2791e34fc67e6544efde4bbbee4ed0b16d
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-05-18 17:52:35 -0300

    new server

--- lib/services/appwrite_constants.dart
+++ lib/services/appwrite_constants.dart
@@ -1,4 +1,4 @@
 const String endpoint = 'https://console.brunoalm.ml/v1';
-const String projectId = '6435e66b09c6f7b2b64a';
+const String projectId = '6465339c8780acd51bd0';
 const String databaseId = '64371957cdee5a18b5c0';
 const String collectionId = '6437196016dba282d056';
