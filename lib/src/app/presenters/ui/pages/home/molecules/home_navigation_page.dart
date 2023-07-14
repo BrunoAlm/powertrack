4b06e5c181d14b791544ea2049457b9bb1b07abd
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-07-14 00:06:15 -0300

    fix layout

--- lib/src/app/presenters/ui/pages/home/molecules/home_navigation_page.dart
+++ lib/src/app/presenters/ui/pages/home/molecules/home_navigation_page.dart
@@ -22,103 +22,134 @@ class HomeNavigationPageState extends State<HomeNavigationPage> {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: OverallConsumptionCard(
-              equipmentsController: widget.equipmentsStore,
-              loginController: widget.loginController,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: OverallConsumptionCard(
+                equipmentsController: widget.equipmentsStore,
+                loginController: widget.loginController,
+              ),
             ),
-          ),
-          const SizedBox(height: 40),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16, 0, 0, 0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Consumo',
-                        style: Theme.of(context).textTheme.titleLarge!),
-                    Container(
-                      height: 1.5,
-                      width: 120,
-                      color: Theme.of(context).colorScheme.onSurfaceVariant,
-                    ),
-                  ],
+            const SizedBox(height: 20),
+            Visibility(
+              visible: widget.equipmentsStore.loadedEquipments.isNotEmpty,
+              replacement: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Adicione equipamentos para visualizar os dados 😁',
+                  style: Theme.of(context).textTheme.titleLarge,
+                  textAlign: TextAlign.center,
                 ),
-                const SizedBox(height: 10),
-                SizedBox(
-                  height: 180,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: widget.equipmentsStore.loadedEquipments.length,
-                    padding: const EdgeInsets.only(right: 10),
-                    itemBuilder: (context, index) => Container(
-                      margin: const EdgeInsets.only(right: 10, bottom: 10),
-                      child: IndividualEquipmentCard(
-                        data:
-                            '${Helper.formatDouble(widget.equipmentsStore.calculateIndividualConsumption(
-                          widget.equipmentsStore.loadedEquipments[index].name,
-                        ))}kWh',
-                        name:
-                            widget.equipmentsStore.loadedEquipments[index].name,
-                        qty: widget.equipmentsStore.individualTotalQty(widget
-                            .equipmentsStore.loadedEquipments[index].name),
-                        icon: Icons.bolt_rounded,
+              ),
+              child: Column(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Consumo',
+                                style: Theme.of(context).textTheme.titleLarge!),
+                            Container(
+                              height: 1.5,
+                              width: 120,
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .onSurfaceVariant,
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16, 0, 0, 0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Tempo de uso',
-                        style: Theme.of(context).textTheme.titleLarge!),
-                    Container(
-                      height: 1.5,
-                      width: 160,
-                      color: Theme.of(context).colorScheme.onSurfaceVariant,
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                SizedBox(
-                  height: 180,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: widget.equipmentsStore.loadedEquipments.length,
-                    padding: const EdgeInsets.only(right: 10),
-                    itemBuilder: (context, index) => Container(
-                      margin: const EdgeInsets.only(right: 10, bottom: 10),
-                      child: IndividualEquipmentCard(
-                        data:
-                            '${widget.equipmentsStore.individualTime(widget.equipmentsStore.loadedEquipments[index].name)}h',
-                        name:
-                            widget.equipmentsStore.loadedEquipments[index].name,
-                        qty: widget.equipmentsStore.individualTotalQty(widget
-                            .equipmentsStore.loadedEquipments[index].name),
-                        icon: Icons.timer_outlined,
+                      const SizedBox(height: 5),
+                      SizedBox(
+                        height: 150,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount:
+                              widget.equipmentsStore.loadedEquipments.length,
+                          padding: const EdgeInsets.only(
+                              right: 16, left: 16, top: 5),
+                          itemBuilder: (context, index) => Container(
+                            margin:
+                                const EdgeInsets.only(right: 10, bottom: 10),
+                            child: IndividualEquipmentCard(
+                              data:
+                                  '${Helper.formatDouble(widget.equipmentsStore.calculateIndividualConsumption(
+                                widget.equipmentsStore.loadedEquipments[index]
+                                    .name,
+                              ))}kWh',
+                              name: widget
+                                  .equipmentsStore.loadedEquipments[index].name,
+                              qty: widget.equipmentsStore.individualTotalQty(
+                                  widget.equipmentsStore.loadedEquipments[index]
+                                      .name),
+                              icon: Icons.bolt_rounded,
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ),
-              ],
+                  const SizedBox(height: 20),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Tempo de uso',
+                                style: Theme.of(context).textTheme.titleLarge!),
+                            Container(
+                              height: 1.5,
+                              width: 160,
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .onSurfaceVariant,
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      SizedBox(
+                        height: 150,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount:
+                              widget.equipmentsStore.loadedEquipments.length,
+                          padding: const EdgeInsets.only(
+                              right: 16, left: 16, top: 5),
+                          itemBuilder: (context, index) => Container(
+                            margin:
+                                const EdgeInsets.only(right: 10, bottom: 10),
+                            child: IndividualEquipmentCard(
+                              data:
+                                  '${widget.equipmentsStore.individualTime(widget.equipmentsStore.loadedEquipments[index].name)}h',
+                              name: widget
+                                  .equipmentsStore.loadedEquipments[index].name,
+                              qty: widget.equipmentsStore.individualTotalQty(
+                                  widget.equipmentsStore.loadedEquipments[index]
+                                      .name),
+                              icon: Icons.timer_outlined,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
