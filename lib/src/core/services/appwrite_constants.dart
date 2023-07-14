57abf5e4a1ff49a9ea22b1c9f04c888daecd670b
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-07-14 00:03:58 -0300

    add avatarBucket

--- lib/src/core/services/appwrite_constants.dart
+++ lib/src/core/services/appwrite_constants.dart
@@ -3,3 +3,4 @@ const String projectId = '6465339c8780acd51bd0';
 const String databaseId = '64371957cdee5a18b5c0';
 const String equipmentsCollectionId = '6437196016dba282d056';
 const String userPrefsCollectionId = '6483d6da8de5ebb2c005';
+const String avatarBucket = '64b0b342271800dd726d';
