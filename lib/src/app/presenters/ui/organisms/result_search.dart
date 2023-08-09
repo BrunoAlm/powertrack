d6114feee8f41af8d064715dbff2dd3fce0b0af6
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-08-09 01:16:59 -0300

    Update result_search.dart

--- lib/src/app/presenters/ui/organisms/result_search.dart
+++ lib/src/app/presenters/ui/organisms/result_search.dart
@@ -1,50 +1,71 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-import 'package:uitcc/src/app/presenters/ui/atom/equipments_raw_list.dart';
+import 'package:logger/logger.dart';
+import 'package:uitcc/src/app/domain/entities/equipment_model.dart';
 import 'package:uitcc/src/app/presenters/controllers/equipments_controller.dart';
+import 'package:uitcc/src/app/presenters/ui/atom/equipments_raw_list.dart';
+import 'package:uitcc/src/app/presenters/ui/molecules/add_equipment_bottomsheet.dart';
+import 'package:uitcc/src/core/services/helpers/helper.dart';
 
-class ResultSearch extends StatefulWidget {
-  final EquipmentsController equipmentStore;
+class ResultSearch extends StatelessWidget {
+  final EquipmentsController ct;
+
   const ResultSearch({
     Key? key,
-    required this.equipmentStore,
+    required this.ct,
   }) : super(key: key);
 
   @override
-  State<ResultSearch> createState() => _ResultSearchState();
-}
-
-class _ResultSearchState extends State<ResultSearch> {
-  @override
   Widget build(BuildContext context) {
-    List<String> filteredEquipmentsName =
-        widget.equipmentStore.filteredEquipmentsName.value;
+    List<String> filteredEquipmentsName = ct.filteredEquipmentsName.value;
+    var logger = Logger();
     return SizedBox(
-      height: (50 * filteredEquipmentsName.length.toDouble())
-          .clamp(0, 250)
+      height: (65 * filteredEquipmentsName.length.toDouble())
+          .clamp(0, 200)
           .toDouble(),
       width: 300,
       child: ListView.builder(
         itemCount: filteredEquipmentsName.length,
-        padding: const EdgeInsets.only(bottom: 20),
+        padding: const EdgeInsets.fromLTRB(0, 5, 0, 10),
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(
-              filteredEquipmentsName[index],
-            ),
-            trailing: IconButton(
-              onPressed: () {
-                widget.equipmentStore.add(
-                  name: filteredEquipmentsName[index],
-                  qty: 1,
-                  time: const TimeOfDay(hour: 00, minute: 00),
-                  power: TextEditingController(text: '0'),
-                );
-                equipmentsRawList.removeWhere(
-                    (element) => element == filteredEquipmentsName[index]);
-                widget.equipmentStore.performSearch();
-              },
-              icon: const Icon(Icons.add),
+          return Container(
+            margin: const EdgeInsets.only(top: 5),
+            decoration: ShapeDecoration(
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
+                color: Theme.of(context).colorScheme.background),
+            child: ListTile(
+              dense: true,
+              title: Text(
+                filteredEquipmentsName[index],
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(fontWeight: FontWeight.bold),
+              ),
+              trailing: IconButton(
+                onPressed: () {
+                  ct.searchEC.clear();
+                  showDialog(
+                    context: context,
+                    builder: (context) => AddEquipmentBottomSheet(
+                      equip: EquipmentModel(
+                        id: Helper.idGenerator(),
+                        name: filteredEquipmentsName[index],
+                        qty: 1,
+                        power: 0,
+                        time: DateTime.utc(2023),
+                      ),
+                      ct: ct,
+                    ),
+                  ).then((value) {
+                    logger.d('log: $value');
+                    equipmentsRawList.removeWhere(
+                        (element) => element == filteredEquipmentsName[index]);
+                  });
+                },
+                icon: const Icon(Icons.add),
+              ),
             ),
           );
         },
