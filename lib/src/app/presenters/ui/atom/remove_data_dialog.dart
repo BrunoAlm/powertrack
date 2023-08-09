073a7bb73c0304f15051f4751b2b8d3ce8eb2065
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-08-09 01:11:58 -0300

    Create remove_data_dialog.dart

---
+++ lib/src/app/presenters/ui/atom/remove_data_dialog.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:uitcc/src/app/presenters/controllers/equipments_controller.dart';
+import 'package:uitcc/src/core/services/helpers/helper.dart';
+
+class RemoveDataDialog extends StatelessWidget {
+  final EquipmentsController equipmentsCt;
+  const RemoveDataDialog({super.key, required this.equipmentsCt});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Remover dados'),
+      content: RichText(
+        text: TextSpan(
+          text: 'Ao clicar em confirmar, você vai ',
+          style: Theme.of(context).textTheme.bodyMedium,
+          children: [
+            TextSpan(
+              text: 'remover',
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.error,
+                  ),
+            ),
+            TextSpan(
+              text: ' seus equipamentos cadastrados',
+              style: Theme.of(context).textTheme.bodyMedium,
+            )
+          ],
+        ),
+      ),
+      actions: [
+        ElevatedButton(
+          onPressed: () => Navigator.pop(context),
+          child: Text(
+            'Cancelar',
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+        ),
+        Container(
+          decoration: ShapeDecoration(
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(22))),
+            shadows: ThemeHelper.shadow(context),
+          ),
+          child: ElevatedButton(
+            onPressed: () {
+              equipmentsCt.deleteAllDocuments();
+              Navigator.pop(context);
+            },
+            child: Text(
+              'Confirmar',
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
