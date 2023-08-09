eb968f8466ae841a9d34042640e108ac573b9bd8
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-08-09 01:18:11 -0300

    add ActiveButton

--- lib/src/app/presenters/ui/organisms/register_equipments_page.dart
+++ lib/src/app/presenters/ui/organisms/register_equipments_page.dart
@@ -1,41 +1,48 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:uitcc/src/app/presenters/ui/atom/active_button.dart';
 import 'package:uitcc/src/app/presenters/ui/organisms/added_equipments.dart';
 import 'package:uitcc/src/app/presenters/ui/organisms/result_search.dart';
 import 'package:uitcc/src/app/presenters/ui/atom/search_text_field.dart';
 import 'package:uitcc/src/app/presenters/ui/states/search_equipment_state.dart';
 import 'package:uitcc/src/app/presenters/controllers/equipments_controller.dart';
 
-class RegisterEquipments extends StatefulWidget {
-  const RegisterEquipments({
+class RegisterEquipmentsPage extends StatefulWidget {
+  const RegisterEquipmentsPage({
     Key? key,
   }) : super(key: key);
   @override
-  RegisterEquipmentsState createState() => RegisterEquipmentsState();
+  RegisterEquipmentsPageState createState() => RegisterEquipmentsPageState();
 }
 
-class RegisterEquipmentsState extends State<RegisterEquipments> {
+class RegisterEquipmentsPageState extends State<RegisterEquipmentsPage> {
   @override
   Widget build(BuildContext context) {
-    var ct = Modular.get<EquipmentsController>();
+    var equipmentCt = Modular.get<EquipmentsController>();
     return SafeArea(
       child: Scaffold(
         body: Padding(
-          padding: const EdgeInsets.fromLTRB(38.0, 38.0, 38.0, 20),
+          padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const SizedBox(height: 20),
-              Text(
-                'Quais destes equipamentos você tem em casa?',
-                style: Theme.of(context).textTheme.headlineSmall,
+              Center(
+                child: Text(
+                  'Adicione seus equipamentos',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineSmall!
+                      .copyWith(fontWeight: FontWeight.bold),
+                ),
               ),
               const SizedBox(height: 20),
               ValueListenableBuilder(
-                valueListenable: ct.searchState,
+                valueListenable: equipmentCt.searchState,
                 builder: (context, SearchEquipmentState state, child) =>
                     Container(
-                  padding: const EdgeInsets.all(10),
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
                   decoration: BoxDecoration(
                     color: state is SuccessSearchEquipmentState ||
                             state is FailedSearchEquipmentState
@@ -50,49 +57,56 @@ class RegisterEquipmentsState extends State<RegisterEquipments> {
                       SizedBox(
                         width: 300,
                         child: SearchTextField(
-                          equipmentsStore: ct,
+                          equipmentsStore: equipmentCt,
                         ),
                       ),
                       state is SuccessSearchEquipmentState
-                          ? ResultSearch(equipmentStore: ct)
+                          ? ResultSearch(
+                              ct: equipmentCt,
+                            )
                           : state is FailedSearchEquipmentState
                               ? const Padding(
                                   padding: EdgeInsets.all(20.0),
-                                  child: Text('Equipamento não cadastrado.'),
+                                  child: Text('Equipamento não encontrado'),
                                 )
                               : const SizedBox(),
                     ],
                   ),
                 ),
               ),
-              AddedEquipments(equipmentsStore: ct),
-              SizedBox(
-                child: BackButton(
-                  onPressed: () {
-                    var toBeAdded = ct.equipmentsToBeAdded;
-                    if (!ct.equipmentIsValid(toBeAdded)) {
-                      ct.compareAndCreateDocument(toBeAdded);
-                      Modular.to.pop();
-                    } else {
-                      showDialog(
-                        context: context,
-                        builder: (context) => AlertDialog(
-                          title: Text(
-                            'Preencha corretamente os campos',
-                            style: Theme.of(context).textTheme.bodyLarge,
-                          ),
-                          actions: [
-                            ElevatedButton(
-                              onPressed: () => Navigator.pop(context),
-                              child: const Text('Ok'),
-                            )
-                          ],
-                        ),
-                      );
-                    }
-                  },
-                ),
+              const SizedBox(height: 10),
+              AddedEquipments(equipmentsCt: equipmentCt),
+            ],
+          ),
+        ),
+        bottomNavigationBar: Container(
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom + 20,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ActiveButton(
+                onTap: () => Navigator.of(context).pop(),
+                text: 'Voltar',
+                icon: Icons.arrow_back,
+                position: ActiveButtonPosition.left,
               ),
+              ActiveButton(
+                onTap: () {
+                  equipmentCt
+                      .compareAndCreateDocument(equipmentCt.equipmentsToBeAdded)
+                      .then(
+                    (value) {
+                      equipmentCt.equipmentsToBeAdded.clear();
+                      Navigator.of(context).pop();
+                    },
+                  );
+                },
+                text: 'Salver',
+                position: ActiveButtonPosition.right,
+                icon: Icons.save_outlined,
+              )
             ],
           ),
         ),
