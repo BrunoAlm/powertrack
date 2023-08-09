9145db221deaf4b8cb1db7623be221bc1563c0f5
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-08-09 01:16:12 -0300

    add ChangeTaxDialog and RemoveDataDialog

--- lib/src/app/presenters/ui/molecules/settings_drawer.dart
+++ lib/src/app/presenters/ui/molecules/settings_drawer.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:uitcc/src/app/presenters/controllers/equipments_controller.dart';
+import 'package:uitcc/src/app/presenters/controllers/home_store.dart';
 import 'package:uitcc/src/app/presenters/controllers/login_controller.dart';
+import 'package:uitcc/src/app/presenters/ui/atom/change_tax_dialog.dart';
+import 'package:uitcc/src/app/presenters/ui/atom/remove_data_dialog.dart';
 import 'package:uitcc/src/core/services/helpers/helper.dart';
 
 class SettingsDrawer extends StatefulWidget {
   final LoginController loginCt;
+  final HomeStore homeStore;
   final EquipmentsController equipmentsCt;
   const SettingsDrawer({
     Key? key,
     required this.loginCt,
     required this.equipmentsCt,
+    required this.homeStore,
   }) : super(key: key);
 
   @override
@@ -18,15 +23,6 @@ class SettingsDrawer extends StatefulWidget {
 }
 
 class SettingsDrawerState extends State<SettingsDrawer> {
-  late TextEditingController taxEC;
-
-  @override
-  void initState() {
-    super.initState();
-    taxEC =
-        TextEditingController(text: widget.loginCt.userPrefs.tax.toString());
-  }
-
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -107,89 +103,11 @@ class SettingsDrawerState extends State<SettingsDrawer> {
               children: [
                 GestureDetector(
                   onTap: () => showDialog(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                            title: const Text('Alterar taxa'),
-                            content: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                const Text(
-                                  'A taxa é o valor que a sua distribuidora de energia cobra por kWh consumido.',
-                                ),
-                                const SizedBox(height: 20),
-                                TextField(
-                                  controller: taxEC,
-                                  decoration: InputDecoration(
-                                    hintText: 'Insira o valor',
-                                    enabledBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Theme.of(context).hintColor,
-                                      ),
-                                      borderRadius: BorderRadius.circular(20),
-                                    ),
-                                    prefixIcon: const Icon(Icons.receipt),
-                                    focusedBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Theme.of(context).hintColor,
-                                      ),
-                                      borderRadius: BorderRadius.circular(20),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            actions: [
-                              ElevatedButton(
-                                onPressed: () => Navigator.pop(context),
-                                child: Text(
-                                  'Cancelar',
-                                  style: Theme.of(context).textTheme.bodyMedium,
-                                ),
-                              ),
-                              Container(
-                                decoration: ShapeDecoration(
-                                  shape: const RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(22))),
-                                  shadows: ThemeHelper.shadow(context),
-                                ),
-                                child: ElevatedButton(
-                                  onPressed: () {
-                                    double? taxValue = double.tryParse(
-                                        taxEC.text.replaceAll(',', '.'));
-                                    if (taxValue != null) {
-                                      widget.loginCt.updateTax(taxValue);
-                                      Navigator.pop(context);
-                                    } else {
-                                      showDialog(
-                                        context: context,
-                                        builder: (context) => AlertDialog(
-                                          title: const Text(
-                                              'coloque um valor válido'),
-                                          actions: [
-                                            ElevatedButton(
-                                              onPressed: () => Modular.to.pop(),
-                                              child: Text(
-                                                'OK',
-                                                style: Theme.of(context)
-                                                    .textTheme
-                                                    .bodyMedium,
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      );
-                                    }
-                                  },
-                                  child: Text(
-                                    'Alterar',
-                                    style:
-                                        Theme.of(context).textTheme.bodyMedium,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          )),
+                    context: context,
+                    builder: (context) => ChangeTaxDialog(
+                      loginCt: widget.loginCt,
+                    ),
+                  ),
                   child: const Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
@@ -207,57 +125,8 @@ class SettingsDrawerState extends State<SettingsDrawer> {
                   onTap: () {
                     showDialog(
                       context: context,
-                      builder: (context) => AlertDialog(
-                        title: const Text('Remover dados'),
-                        content: RichText(
-                          text: TextSpan(
-                            text: 'Ao clicar em confirmar, você vai ',
-                            style: Theme.of(context).textTheme.bodyMedium,
-                            children: [
-                              TextSpan(
-                                text: 'remover',
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyMedium!
-                                    .copyWith(
-                                      color:
-                                          Theme.of(context).colorScheme.error,
-                                    ),
-                              ),
-                              TextSpan(
-                                text: ' seus equipamentos cadastrados',
-                                style: Theme.of(context).textTheme.bodyMedium,
-                              )
-                            ],
-                          ),
-                        ),
-                        actions: [
-                          ElevatedButton(
-                            onPressed: () => Navigator.pop(context),
-                            child: Text(
-                              'Cancelar',
-                              style: Theme.of(context).textTheme.bodyMedium,
-                            ),
-                          ),
-                          Container(
-                            decoration: ShapeDecoration(
-                              shape: const RoundedRectangleBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(22))),
-                              shadows: ThemeHelper.shadow(context),
-                            ),
-                            child: ElevatedButton(
-                              onPressed: () {
-                                widget.equipmentsCt.deleteAllDocuments();
-                                Modular.to.pop();
-                              },
-                              child: Text(
-                                'Confirmar',
-                                style: Theme.of(context).textTheme.bodyMedium,
-                              ),
-                            ),
-                          ),
-                        ],
+                      builder: (context) => RemoveDataDialog(
+                        equipmentsCt: widget.equipmentsCt,
                       ),
                     );
                   },
