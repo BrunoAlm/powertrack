629156e2d44004c8ac5fbef9c6e6648cfb17c6a6
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-08-16 23:01:52 -0300

    update ListViewEquipmentData

--- lib/src/features/app/features/home/presenter/ui/molecules/listview_equipment_card.dart
+++ lib/src/features/app/features/home/presenter/ui/molecules/listview_equipment_card.dart
@@ -1,52 +1,107 @@
 import 'package:flutter/material.dart';
 import 'package:uitcc/src/features/app/features/equipments/domain/entity/equipment_model.dart';
 import 'package:uitcc/src/core/services/helpers/helper.dart';
+import 'package:uitcc/src/features/app/features/equipments/presenters/controllers/equipments_controller.dart';
+import 'package:uitcc/src/features/login/presenters/controllers/login_controller.dart';
 
-enum CardType { time, consumption, cost }
+enum CardType { time, consumption, cost, qty }
 
 class ListViewEquipmentData extends StatelessWidget {
   final String title;
-  final int itemCount;
-  final IconData cardIcon;
-  final List<EquipmentModel> equipments;
+  final EquipmentsController equipCt;
+  final LoginController loginCt;
   final CardType cardType;
-  final Function(String)? calcFunc;
-  final double? tax;
-  final Function(String, double)? calcCost;
 
   const ListViewEquipmentData({
     Key? key,
     required this.title,
-    required this.itemCount,
-    required this.cardIcon,
-    required this.equipments,
+    required this.equipCt,
     required this.cardType,
-    this.calcFunc,
-    this.tax,
-    this.calcCost,
+    required this.loginCt,
   }) : super(key: key);
 
-  static String getValue(
-    CardType cardType,
-    EquipmentModel equipment,
-    Function(String)? calcFunc,
-    double? tax,
-    Function(String, double)? calcCost,
-  ) {
+  String getValue({
+    required CardType cardType,
+    required List<EquipmentModel> sortedEquipments,
+    required int index,
+    required LoginController loginCt,
+  }) {
+    final tax = loginCt.userPrefs.tax;
+    final equipment = sortedEquipments[index];
+
     switch (cardType) {
       case CardType.consumption:
-        return '${Helper.formatDouble(calcFunc!(equipment.name))}kWh';
+        double result = equipCt.individualConsumption(equipment.name);
+        return '${Helper.formatDouble(result)} kWh';
+      case CardType.cost:
+        double cost = equipCt.individualCost(equipment.name, tax);
+        String result = 'R\$ ${Helper.formatDouble(cost)}';
+        if (cost == 0) {
+          result = 'Taxa inválida';
+        }
+        return result;
       case CardType.time:
         return Helper.formatHour(equipment.time);
-      case CardType.cost:
-        return 'R\$ ${Helper.formatDouble(calcCost!(equipment.name, tax!))}';
+
+      case CardType.qty:
+        return equipment.qty.toString();
       default:
         return '';
     }
   }
 
+  IconData getIcon(CardType cardType) {
+    switch (cardType) {
+      case CardType.consumption:
+        return Icons.bolt_rounded;
+      case CardType.cost:
+        return Icons.attach_money_rounded;
+      case CardType.time:
+        return Icons.access_time_filled_sharp;
+      case CardType.qty:
+        return Icons.stacked_bar_chart_outlined;
+      default:
+        return Icons.circle_outlined;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<EquipmentModel> equipments = equipCt.loadedEquipments;
+
+    // Obtenha a lista de equipamentos ordenada com base no tipo de card
+    List<EquipmentModel> sortedEquipments;
+
+    // Taxa da concessionaria do usuario
+    double tax = loginCt.userPrefs.tax;
+
+    switch (cardType) {
+      case CardType.consumption:
+        sortedEquipments = [...equipments]..sort((a, b) {
+            double consumptionA = equipCt.individualConsumption(a.name);
+            double consumptionB = equipCt.individualConsumption(b.name);
+            return consumptionB.compareTo(consumptionA);
+          });
+        break;
+      case CardType.cost:
+        sortedEquipments = [...equipments]..sort((a, b) {
+            double costA = equipCt.individualCost(a.name, tax);
+            double costB = equipCt.individualCost(b.name, tax);
+            return costB.compareTo(costA);
+          });
+        break;
+      case CardType.time:
+        sortedEquipments = [...equipments]
+          ..sort((a, b) => a.time.compareTo(b.time));
+        break;
+      case CardType.qty:
+        sortedEquipments = [...equipments]
+          ..sort((a, b) => a.qty.compareTo(b.qty));
+        break;
+      default:
+        sortedEquipments = equipments;
+    }
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -69,11 +124,19 @@ class ListViewEquipmentData extends StatelessWidget {
           height: 150,
           child: ListView.builder(
             scrollDirection: Axis.horizontal,
-            itemCount: itemCount,
+            itemCount: equipments.length,
             padding: const EdgeInsets.only(right: 16, left: 16, top: 5),
             itemBuilder: (context, index) {
-              var name = equipments[index].name;
-              var qty = equipments[index].qty;
+              String name = equipments[index].name;
+              int qty = equipments[index].qty;
+
+              String value = getValue(
+                cardType: cardType,
+                sortedEquipments: sortedEquipments,
+                index: index,
+                loginCt: loginCt,
+              );
+
               return Container(
                 margin: const EdgeInsets.only(right: 10, bottom: 10),
                 child: Container(
@@ -94,8 +157,8 @@ class ListViewEquipmentData extends StatelessWidget {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Container(
-                              height: 34,
-                              width: 34,
+                              height: 32,
+                              width: 32,
                               decoration: ShapeDecoration(
                                 shape: const CircleBorder(),
                                 color: Theme.of(context)
@@ -104,7 +167,7 @@ class ListViewEquipmentData extends StatelessWidget {
                                     .withOpacity(.8),
                               ),
                               child: Icon(
-                                cardIcon,
+                                getIcon(cardType),
                                 color: Theme.of(context).colorScheme.surface,
                               ),
                             ),
@@ -117,22 +180,12 @@ class ListViewEquipmentData extends StatelessWidget {
                                   Text(
                                     '${qty}x $name',
                                     style:
-                                        Theme.of(context).textTheme.bodyLarge,
+                                        Theme.of(context).textTheme.bodyMedium,
                                   ),
                                   Text(
-                                    getValue(
-                                      cardType,
-                                      equipments[index],
-                                      calcFunc,
-                                      tax,
-                                      calcCost,
-                                    ),
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .bodyLarge!
-                                        .copyWith(
-                                          fontWeight: FontWeight.w500,
-                                        ),
+                                    value,
+                                    style:
+                                        Theme.of(context).textTheme.bodyMedium!,
                                   ),
                                 ],
                               ),
